33445a437fbee5a68f49512e464dba05f767dbd2
Isaias Pomales <impomales@gmail.com>
2018-10-25 17:16:20 -0400

    fetch currently logged in user

--- client/components/App.js
+++ client/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
@@ -8,17 +8,44 @@ import SignIn from './SignIn';
 import Register from './Register';
 import NoMatch from '../containers/NoMatch';
 
-const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Portfolio} />
-      <Route path="/portfolio" component={Portfolio} />
-      <Route path="/history" component={Transactions} />
-      <Route path="/signin" component={SignIn} />
-      <Route path="/register" component={Register} />
-      <Route component={NoMatch} />
-    </Switch>
-  </Router>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {}
+    }
+  }
+
+  componentDidMount() {
+    this.fetchUserId();
+  }
+
+  fetchUserId() {
+    fetch('/auth/me')
+      .then(res => res.json())
+      .then(user => this.setState({ user }));
+  }
+
+  isLoggedIn() {
+    return this.state.user && this.state.user.id;
+  }
+
+  render() {
+    console.log(this.state.user);
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Portfolio} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/history" component={Transactions} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/register" component={Register} />
+          <Route component={NoMatch} />
+        </Switch>
+      </Router>
+    );
+  }
+}
 
 export default App;
