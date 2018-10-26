fc874310f2891aa6a3c741e2602d10f7d6e783b5
Isaias Pomales <impomales@gmail.com>
2018-10-26 00:04:03 -0400

    store api url

--- client/components/Portfolio.js
+++ client/components/Portfolio.js
@@ -1,10 +1,19 @@
 import React from 'React';
 import Nav from '../containers/Nav';
 
+const apiUrl = 'https://api.iextrading.com/1.0';
+
 const Portfolio = ({ updateUser }) => (
   <div>
     <Nav updateUser={updateUser} />
     <h1>Portfolio</h1>
+    <footer>
+      <p>
+        Data provided for free by{' '}
+        <a href="https://iextrading.com/developer/">IEX</a>. View{' '}
+        <a href="https://iextrading.com/api-exhibit-a/">IEX’s Terms of Use</a>.
+      </p>
+    </footer>
   </div>
 );
 
