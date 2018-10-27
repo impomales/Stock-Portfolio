00017b9a681c2321e445436fa1cd59be03014ad7
Isaias Pomales <impomales@gmail.com>
2018-10-26 21:53:31 -0400

    define function to format currency display

---
+++ client/util/index.js
@@ -0,0 +1,10 @@
+const formatCurrency = num => {
+  return num.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+}
+
+export { formatCurrency };
